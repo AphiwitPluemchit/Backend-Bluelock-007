ca8dc9ef62cfa193841f8419d0a51f63fc2e0a5e
SahaphapM <65160309@go.buu.ac.th>
2025-05-12 12:51:18 +0700

    fix: if not use redis then not connect it

--- src/main.go
+++ src/main.go
@@ -25,9 +25,9 @@ func main() {
 		appURI = "8888" // ใช้ 8888 เป็นค่าเริ่มต้น
 	}
 	redisURI := os.Getenv("REDIS_URI")
-	if redisURI == "" {
-		redisURI = "localhost:6379" // ใช้ localhost:6379 เป็นค่าเริ่มต้น
-	}
+	// if redisURI == "" {
+	// 	redisURI = "localhost:6379" // ใช้ localhost:6379 เป็นค่าเริ่มต้น
+	// }
 	origins := os.Getenv("ALLOWED_ORIGINS") // ✅ เปิดใช้งาน CORS Middleware
 	if origins == "" {
 		origins = "*"
@@ -50,24 +50,26 @@ func main() {
 	}))
 
 	// ✅ สร้าง Asynq Client และเริ่มรัน Asynq Worker
-	services.AsynqClient = asynq.NewClient(asynq.RedisClientOpt{Addr: redisURI})
+	if redisURI != "" {
+		services.AsynqClient = asynq.NewClient(asynq.RedisClientOpt{Addr: redisURI})
 
-	go func() {
-		srv := asynq.NewServer(
-			asynq.RedisClientOpt{Addr: redisURI},
-			asynq.Config{
-				Concurrency: 10, // รันพร้อมกันได้ 10 task
-			},
-		)
-		mux := asynq.NewServeMux()
-		mux.HandleFunc(jobs.TypeCloseActivity, jobs.HandleCloseActivityTask)
+		go func() {
+			srv := asynq.NewServer(
+				asynq.RedisClientOpt{Addr: redisURI},
+				asynq.Config{
+					Concurrency: 10, // รันพร้อมกันได้ 10 task
+				},
+			)
+			mux := asynq.NewServeMux()
+			mux.HandleFunc(jobs.TypeCloseActivity, jobs.HandleCloseActivityTask)
 
-		if err := srv.Run(mux); err != nil {
-			log.Fatal("❌ Failed to start Asynq worker:", err)
-		} else {
-			log.Println("🚀 Asynq Worker is starting...")
-		}
-	}()
+			if err := srv.Run(mux); err != nil {
+				log.Fatal("❌ Failed to start Asynq worker:", err)
+			} else {
+				log.Println("🚀 Asynq Worker is starting...")
+			}
+		}()
+	}
 
 	// เปิดใช้งาน Swagger ที่ URL /swagger
 	app.Get("/swagger/*", swagger.HandlerDefault)
