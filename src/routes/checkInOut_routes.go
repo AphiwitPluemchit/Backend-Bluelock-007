c426943c311ea85d35d31998199b6523f9a76488
aphiwitrr <168804537+aphiwitrr@users.noreply.github.com>
2025-09-11 21:24:04 +0700

    chore: enable JWT authentication middleware for CheckInOut routes

--- src/routes/checkInOut_routes.go
+++ src/routes/checkInOut_routes.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"Backend-Bluelock-007/src/controllers"
+	"Backend-Bluelock-007/src/middleware"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -9,7 +10,7 @@ import (
 // CheckInOutRoutes กำหนดเส้นทางสำหรับ CheckInOut API
 func checkInOutRoutes(router fiber.Router) {
 	checkInOutRoutes := router.Group("/checkInOuts")
-	// checkInOutRoutes.Use(middleware.AuthJWT)
+	checkInOutRoutes.Use(middleware.AuthJWT)
 	// checkInOutRoutes.Post("/generate-link", controllers.GenerateLink)
 	// checkInOutRoutes.Post("/checkin/:uuid", controllers.Checkin)   // ดึงผู้ใช้ทั้งหมด
 	// checkInOutRoutes.Post("/checkout/:uuid", controllers.Checkout) // ดึงผู้ใช้ทั้งหมด
